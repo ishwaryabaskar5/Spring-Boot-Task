a3fa4b4148a59ac9965575b39dac0daeb577f105
isbaska <isbaska1@in.ibm.com>
2019-07-24 11:24:35 +0530

    Springboot task step-1

---
+++ README.md
@@ -0,0 +1,7 @@
+                    Spring Boot Task
+
+-------------------------------------------------------------------------------------------------
+
+
+1. Complete all the endpoints for CRUD operations on Muzix
+
---
+++ src/main/java/com/stackroute/muzix/service/TrackServiceImpl.java
@@ -0,0 +1,46 @@
+package com.stackroute.muzix.service;
+
+import com.stackroute.muzix.model.Track;
+import com.stackroute.muzix.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TrackServiceImpl implements TrackService {
+	
+	TrackRepository trackRepository;
+	
+	@Autowired
+	public TrackServiceImpl(TrackRepository trackRepository) {
+		this.trackRepository = trackRepository;
+	}
+	
+	@Override
+	public boolean saveTrack(Track track) {
+			trackRepository.save(track);
+			return true;
+	}
+	
+	@Override
+	public boolean deleteTrack(int id) {
+			trackRepository.deleteById(id);
+			return true;
+	}
+	
+	@Override
+	public List<Track> getAllTracks() {
+		List<Track> trackList = trackRepository.findAll();
+		return trackList;
+	}
+	
+	@Override
+	public boolean updateTrack(int id, Track track) {
+			if(trackRepository.existsById(id) == true){
+				trackRepository.save(track);
+				return true;
+			}
+		return false;
+	}
+}
