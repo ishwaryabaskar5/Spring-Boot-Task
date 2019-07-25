d3cc34649e84e3affee3463b61d182810faa3ebb
isbaska <isbaska1@in.ibm.com>
2019-07-25 09:25:16 +0530

    Seed data using ApplicationListener and CommandLineRunner Interfaces

--- src/main/java/com/stackroute/muzix/config/ApplicationListenerImpl.java
+++ src/main/java/com/stackroute/muzix/config/ApplicationListenerImpl.java
@@ -1,6 +1,10 @@
 package com.stackroute.muzix.config;
 
+import com.stackroute.muzix.exception.TrackAlreadyExistsException;
+import com.stackroute.muzix.model.Track;
+import com.stackroute.muzix.service.TrackService;
 import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -12,9 +16,19 @@ public class ApplicationListenerImpl implements ApplicationListener<ContextRefre
 	private static final Logger LOG = Logger.getLogger(ApplicationListenerImpl.class);
 	public static int counter;
 	
+	@Autowired
+	private TrackService trackService;
+	
+	
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
 		LOG.info("Increment counter");
 		counter++;
+		try {
+			trackService.saveTrack(new Track(1,"aaa","aaa"));
+			trackService.saveTrack(new Track(2,"bbb","bbb"));
+		} catch (TrackAlreadyExistsException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/com/stackroute/muzix/config/CommandLineRunnerImpl.java
+++ src/main/java/com/stackroute/muzix/config/CommandLineRunnerImpl.java
@@ -1,6 +1,10 @@
 package com.stackroute.muzix.config;
 
+import com.stackroute.muzix.exception.TrackAlreadyExistsException;
+import com.stackroute.muzix.model.Track;
+import com.stackroute.muzix.service.TrackService;
 import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -8,10 +12,19 @@ import org.springframework.stereotype.Component;
 public class CommandLineRunnerImpl implements CommandLineRunner {
 	private static final Logger LOG = Logger.getLogger(CommandLineRunnerImpl.class);
 	public static int counter;
-
+	
+	@Autowired
+	private TrackService trackService;
+	
 	@Override
 	public void run(String...args) throws Exception {
 		LOG.info("Increment counter");
 		counter++;
+		try {
+			trackService.saveTrack(new Track(3,"ccc","ccc"));
+			trackService.saveTrack(new Track(4,"ddd","ddd"));
+		} catch (TrackAlreadyExistsException e) {
+			e.printStackTrace();
+		}
 	}
 }
