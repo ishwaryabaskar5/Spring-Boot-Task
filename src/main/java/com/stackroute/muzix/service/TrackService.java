62c7fb6c6cfb62a8c954e94ae969a78572cef591
ishwaryabaskar5 <51943052+ishwaryabaskar5@users.noreply.github.com>
2019-08-07 19:59:53 +0530

    public keyword is removed from interface methods

--- src/main/java/com/stackroute/muzix/service/TrackService.java
+++ src/main/java/com/stackroute/muzix/service/TrackService.java
@@ -7,12 +7,12 @@ import java.util.List;
 //Service interface
 public interface TrackService {
 	
-	public boolean saveTrack(Track track);
+	 boolean saveTrack(Track track);
 	
-	public boolean deleteTrack(int id);
+	 boolean deleteTrack(int id);
 	
-	public List<Track> getAllTracks();
+	 List<Track> getAllTracks();
 	
-	public boolean updateTrack(int id, Track track);
+	 boolean updateTrack(int id, Track track);
 	
 }
